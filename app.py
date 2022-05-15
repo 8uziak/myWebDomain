9513855d260175a69c67e5fc035c9312ac029d69
Mateusz Buziak <matbuziak@gmail.com>
2022-05-15 14:41:06 +0200

    ver0.05

--- app.py
+++ app.py
@@ -1,4 +1,23 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, request, render_template
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
