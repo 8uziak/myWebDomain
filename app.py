c45475ba6b3f5f7702f53bf44ba273127b7e116d
Mateusz Buziak <matbuziak@gmail.com>
2022-05-16 14:17:11 +0200

    ver0.06

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from flask import Flask, redirect, url_for, request, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project.db'
+
+db = SQLAlchemy(app)
+
+class Blogpost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    subtitle = db.Column(db.String(50))
+    date_posted = db.Column(db.DateTime)
+    content = db.Column(db.Text)
 
 @app.route("/")
 def index():
@@ -8,7 +21,34 @@ def index():
 
 @app.route("/projects")
 def projects():
-    return render_template("projects.html")
+
+    posts = Blogpost.query.order_by(Blogpost.date_posted.desc()).all()
+
+    return render_template("projects.html", posts=posts)
+
+@app.route('/projects/<int:post_id>')
+def post(post_id):
+    
+    post = Blogpost.query.filter_by(id=post_id).one()
+
+    return render_template('content.html', post=post)
+
+@app.route('/add')
+def add():
+    return render_template('add.html')
+
+@app.route('/addproject', methods=['POST'])
+def addproject():
+    title = request.form['title']
+    subtitle = request.form['subtitle']
+    content = request.form['content']
+
+    post = Blogpost(title=title, subtitle=subtitle, content=content, date_posted=datetime.now())
+
+    db.session.add(post)
+    db.session.commit()
+
+    return redirect(url_for('projects'))
 
 @app.route("/about")
 def about():
