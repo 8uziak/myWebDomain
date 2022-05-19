e8f72ac91829e8273132577fd6bca07bbb44815a
Mateusz Buziak <matbuziak@gmail.com>
2022-05-19 11:34:09 +0200

    ver0.08

--- app.py
+++ app.py
@@ -2,12 +2,15 @@ from flask import Flask, redirect, url_for, request, render_template
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
+
 app = Flask(__name__)
+
 
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project.db'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://bsqvfgacavyfil:53967cd0e2c8dabdb84436d2cd47845446c3d9db315ea4e565621b8bcdf1d80f@ec2-52-48-159-67.eu-west-1.compute.amazonaws.com:5432/ddakukmsu6ma2m'
 
 db = SQLAlchemy(app)
+
 
 class Blogpost(db.Model):
     id = db.Column(db.Integer, primary_key=True)
