42d74a4181577ea22ec02cb87e85d69587a82471
Mateusz Buziak <matbuziak@gmail.com>
2022-05-20 15:05:25 +0200

    ver0.09

--- app.py
+++ app.py
@@ -63,5 +63,10 @@ def contact():
     return render_template("contact.html")
 
 
+@app.errorhandler(404)
+def not_found(e):
+    return render_template("404.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
