fca2dd835c57a7a90b8458a4964bc38a758798dd
Mateusz Buziak <matbuziak@gmail.com>
2022-06-10 18:20:35 +0200

    ver 0.2 (admin page, login added)

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
-from ast import Pass
-from flask import Flask, redirect, url_for, request, render_template
+from flask import Flask, redirect, url_for, request, render_template, session, abort
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from flask_login import LoginManager , UserMixin, login_required, login_user
-
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 
 app = Flask(__name__)
-# app.secret_key = '#$%RFgfvfvgtredsGFTRDFcxdsrFgfBfdzsadfCVGVde#eR56tR4#eR56Y&' 
+admin = Admin(app)
 
+# DATABASE Configuration
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = "mysecretkeywhichissupposedtobesecret"
+db = SQLAlchemy(app)
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://bsqvfgacavyfil:53967cd0e2c8dabdb84436d2cd47845446c3d9db315ea4e565621b8bcdf1d80f@ec2-52-48-159-67.eu-west-1.compute.amazonaws.com:5432/ddakukmsu6ma2m'
 
-
-db = SQLAlchemy(app)
 
 class Blogpost(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -21,6 +22,41 @@ class Blogpost(db.Model):
     date_posted = db.Column(db.DateTime)
     content = db.Column(db.Text)
 
+class SecureModelView(ModelView):
+    def is_accessible(self):
+        if "logged_in" in session:
+            return True
+        else:
+            abort(403)
+
+admin.add_view(SecureModelView(Blogpost, db.session))
+
+# login to get access to ADMIN functionalities 
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == "POST":
+        if request.form.get("username") == "a" and request.form.get("password") == "a":
+            session["logged_in"] = True
+            return redirect('admin')
+        else:
+            return render_template("login.html", failed=True)
+
+    return render_template("login.html")
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect("login")
+
+
+# ADMIN route only
+# adding projects to database functionality 
+@app.route('/add')
+def add():
+    return render_template('add.html')
+
+
+# user accessible routes 
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -39,10 +75,6 @@ def post(post_id):
 
     return render_template('content.html', post=post)
 
-@app.route('/add')
-@login_required
-def add():
-    return render_template('add.html')
 
 @app.route('/addproject', methods=['POST'])
 def addproject():
