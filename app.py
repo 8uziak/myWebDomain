2ef9c426eeccfdeaaa2771e8f67dfd9a84015239
Mateusz Buziak <matbuziak@gmail.com>
2022-06-11 17:01:47 +0200

    ver 0.21 (message form begin, secure privacy)

--- app.py
+++ app.py
@@ -3,15 +3,30 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
+from flask_mail import Mail, Message
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
 
 app = Flask(__name__)
 admin = Admin(app)
+mail = Mail(app)
 
 # DATABASE Configuration
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = "mysecretkeywhichissupposedtobesecret"
 db = SQLAlchemy(app)
+
+app.config["MAIL_SERVER"] = "smtp-mail.outlook.com "
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USE_SSL"] = False  
+app.config["MAIL_USERNAME"] = os.getenv("mail_user_name") 
+app.config["MAIL_PASSWORD"] = os.getenv("mail_user_password") 
+
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://bsqvfgacavyfil:53967cd0e2c8dabdb84436d2cd47845446c3d9db315ea4e565621b8bcdf1d80f@ec2-52-48-159-67.eu-west-1.compute.amazonaws.com:5432/ddakukmsu6ma2m'
 
 
@@ -35,7 +50,7 @@ admin.add_view(SecureModelView(Blogpost, db.session))
 @app.route('/login', methods=['GET','POST'])
 def login():
     if request.method == "POST":
-        if request.form.get("username") == "a" and request.form.get("password") == "a":
+        if request.form.get("username") == os.getenv("admin_page_login") and request.form.get("password") == os.getenv("admin_page_password"):
             session["logged_in"] = True
             return redirect('admin')
         else:
@@ -48,12 +63,6 @@ def logout():
     session.clear()
     return redirect("login")
 
-
-# ADMIN route only
-# adding projects to database functionality 
-@app.route('/add')
-def add():
-    return render_template('add.html')
 
 
 # user accessible routes 
@@ -93,8 +102,16 @@ def addproject():
 def about():
     return render_template("about.html")
 
-@app.route("/contact")
+@app.route("/contact", methods=['GET','POST'])
 def contact():
+    if request.method == "POST":
+        name = request.form.get('name')
+        email = request.form.get('email')
+        message = request.form.get('message')
+
+        msg = Message(subject=f"Message from {name}, email: {email}", body=message, sender=os.getenv("mail_user_name") , recipients=["matbuziak@gmail.com"])
+        mail.send(msg)
+        return render_template("contact.html", success=True)
     return render_template("contact.html")
 
 
